28616268f89bf7bcb23ebf57a2ccc8990d17559e
rick <rick-prime@rickpublic.rick.rickpublic>
2024-09-04 21:29:03 -0400

    username label render

--- java-login/Login.java
+++ java-login/Login.java
@@ -5,10 +5,24 @@ public class Login {
 	JFrame frame = new JFrame();
 	
 	JPanel panel = new JPanel();
+
+	JLabel userlabel = new JLabel("Username: ");
+	userlabel.setBounds(10, 20, 80, 25);
+	JLabel passlabel = new JLabel("Password: ");
+
+	JTextField userText = new JTextField();
+	userText.setBounds(100, 20, 265, 25);
+	
 	frame.setSize(500, 500);
 	frame.add(panel);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	panel.setLayout(null);
+	panel.add(userlabel);
+	panel.add(passlabel);
+
 	frame.setVisible(true);
+	
     }
     
     public static void main(String[] args) {
