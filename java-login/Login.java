44955283ca8717ce450a320cea888b7c8996854e
rick <rick-prime@rickpublic.rick.rickpublic>
2024-09-07 16:42:11 -0400

    added button to panel and button logic, removed the constructor for button logic.

--- java-login/Login.java
+++ java-login/Login.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Login {
-    public Login() {
+public class Login implements ActionListener {
+    /*public Login() {
 	JFrame frame = new JFrame();
 	
 	JPanel panel = new JPanel();
@@ -16,6 +17,10 @@ public class Login {
 
 	JPasswordField passText = new JPasswordField();
 	passText.setBounds(100, 50, 265, 25);
+
+	JButton loginbutton = new JButton();
+	loginbutton.setBounds(10, 80, 80, 25);
+	loginbutton.addActionListener(new Login());
 	
 	frame.setSize(500, 500);
 	frame.add(panel);
@@ -26,12 +31,49 @@ public class Login {
 	panel.add(passlabel);
 	panel.add(userText);
 	panel.add(passText);
+	panel.add(loginbutton);
+
+	frame.setVisible(true);
+	
+	}*/
+    
+    public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	
+	JPanel panel = new JPanel();
+
+	JLabel userlabel = new JLabel("Username: ");
+	userlabel.setBounds(10, 20, 80, 25);
+	JLabel passlabel = new JLabel("Password: ");
+	passlabel.setBounds(10, 50, 80, 25);
+
+	JTextField userText = new JTextField();
+	userText.setBounds(100, 20, 265, 25);
+
+	JPasswordField passText = new JPasswordField();
+	passText.setBounds(100, 50, 265, 25);
+
+	JButton loginbutton = new JButton();
+	loginbutton.setBounds(10, 80, 80, 25);
+	loginbutton.addActionListener(new Login());
+	
+	frame.setSize(500, 500);
+	frame.add(panel);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	panel.setLayout(null);
+	panel.add(userlabel);
+	panel.add(passlabel);
+	panel.add(userText);
+	panel.add(passText);
+	panel.add(loginbutton);
 
 	frame.setVisible(true);
 	
     }
-    
-    public static void main(String[] args) {
-	new Login();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	System.out.println("Button Clicked.");
     }
 }
